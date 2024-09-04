360d236a1ac6d97cfb0c3a1d2d6b8b01fbf8c80e
ka-amina <ka-amina>
2024-09-05 00:46:42 +0100

    Fonctions

--- day2/Fonctions/ch1.c
+++ day2/Fonctions/ch1.c
@@ -1,9 +1,6 @@
 //Challenge 1 : Fonction de Somme
 //Écrivez une fonction en C qui prend deux entiers en paramètres et retourne leur somme.
 //Créez un programme principal qui utilise cette fonction pour afficher la somme de deux nombres.
-#include<stdio.h>
-#include<stdlib.h>
-
 int somme (int a, int b){
 int s;
 s=a+b;
