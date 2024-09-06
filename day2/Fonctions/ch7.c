7b0242557147ec0ea7887d85bfc527f3bdb0a3c4
ka-amina <ka-amina>
2024-09-06 11:52:41 +0100

    functions

---
+++ day2/Fonctions/ch7.c
@@ -0,0 +1,7 @@
+//Challenge 7 : Fonction d'Inversion de Chaîne
+//Écrivez une fonction en C qui inverse une chaîne de caractères.
+//La fonction doit prendre une chaîne en paramètre et retourner la chaîne inversée.
+//Créez un programme principal qui utilise cette fonction pour afficher une chaîne inversée.
+int Inverse (int a){
+
+}
